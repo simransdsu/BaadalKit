95200205c9a862632e20e026151a26b263de4cf9
simransdsu <simran5590@gmail.com>
2022-02-04 14:11:49 -0800

    made methods public and initializer as well

--- Sources/BaadalKit/BaadalManager.swift
+++ Sources/BaadalKit/BaadalManager.swift
@@ -17,7 +17,7 @@ public struct BaadalManager {
     
     private let database: CKDatabase
     
-    init(identifier: String? = nil) {
+    public init(identifier: String? = nil) {
         if let identifier = identifier {
             database = CKContainer(identifier: identifier).privateCloudDatabase
             return
@@ -28,8 +28,8 @@ public struct BaadalManager {
     
     @available(macOS 10.15.0, *)
     @available(iOS 13.0.0, *)
-    func save(record: CKRecord) async throws -> CKRecord {
- 
+    public func save(record: CKRecord) async throws -> CKRecord {
+        
         return try await withCheckedThrowingContinuation { continutation in
             database.save(record) { record, error in
                 if let record = record, error == nil {
@@ -45,7 +45,7 @@ public struct BaadalManager {
     
     @available(macOS 10.15.0, *)
     @available(iOS 13.0.0, *)
-    func delete(_ item: RecordIDItem) async throws -> CKRecord.ID {
+    public func delete(_ item: RecordIDItem) async throws -> CKRecord.ID {
         return try await withCheckedThrowingContinuation { continutation in
             database.delete(withRecordID: item.recordId) { id, error in
                 if let id = id, error == nil {
@@ -60,44 +60,43 @@ public struct BaadalManager {
     
     @available(macOS 10.15.0, *)
     @available(iOS 13.0.0, *)
-    func fetch(recordType: String) async throws -> [CKRecord]{
+    public func fetch(recordType: String) async throws -> [CKRecord]{
         let query = CKQuery(recordType: recordType,
                             predicate: NSPredicate(value: true))
         
         var records: [CKRecord] = []
         
         return try await withCheckedThrowingContinuation { continutation in
-            if #available(macOS 12.0, *) {
-                if #available(iOS 15.0, *) {
-                    database.fetch(withQuery: query) { result in
-                        
-                        switch(result) {
-                        case .success((let matchResults,_)):
-                            for matchResult in matchResults {
-                                let x = matchResult.1
-                                switch(x) {
-                                case .success(let record):
-                                    records.append(record)
-                                    
-                                case .failure(let err):
-                                    print("❌ Failed to fetch matchedResult from iCloud with error \(err)")
-                                    return continutation.resume(throwing: BaadalError.failedToFetch)
-                                }
-                            }
-                            break
-                            
-                        case .failure(let error):
-                            print("❌ Failed to fetch records from iCloud with error \(error)")
-                            if let ckError = error as? CKError,
-                               ckError.code == .notAuthenticated {
-                                return continutation.resume(throwing: BaadalError.notAuthenticated)
-                            } else {
+            
+            if #available(macOS 12.0, iOS 15.0, *) {
+                database.fetch(withQuery: query) { result in
+                    
+                    switch(result) {
+                    case .success((let matchResults,_)):
+                        for matchResult in matchResults {
+                            let x = matchResult.1
+                            switch(x) {
+                            case .success(let record):
+                                records.append(record)
+                                
+                            case .failure(let err):
+                                print("❌ Failed to fetch matchedResult from iCloud with error \(err)")
                                 return continutation.resume(throwing: BaadalError.failedToFetch)
                             }
                         }
+                        break
                         
-                        return continutation.resume(returning: records)
+                    case .failure(let error):
+                        print("❌ Failed to fetch records from iCloud with error \(error)")
+                        if let ckError = error as? CKError,
+                           ckError.code == .notAuthenticated {
+                            return continutation.resume(throwing: BaadalError.notAuthenticated)
+                        } else {
+                            return continutation.resume(throwing: BaadalError.failedToFetch)
+                        }
                     }
+                    
+                    return continutation.resume(returning: records)
                 }
             }
         }
